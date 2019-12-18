05a52788618e8b4e5def5d863ee54187d16f572e
huangzh <huangzh@gzmoninet.com>
2019-12-18 12:00:51 +0800

    新增网关服务

---
+++ gateway/src/main/java/org/interface21/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package org.interface21.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huangzh
+ * @date 2019-12-18
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
